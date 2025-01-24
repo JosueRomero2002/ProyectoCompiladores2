687511d427f9d1e72661882da2ea4412edb325af
josueromero2002 <josuepinro21@unitec.edu>
2025-01-23 22:02:05 -0600

    main.cpp token error fix

--- main.cpp
+++ main.cpp
@@ -2,18 +2,13 @@
 #include "MiniJavaLexer.hpp"
 #include "tokens.hpp"
 
-// extern MiniJavaLexer lexer;
-
 int main()
 {
-
     MiniJavaLexer lexer;
     Token token;
 
-    while ((token = lexer.lex()) != Token::EndOfFile)
+    while ((token = static_cast<Token>(lexer.lex())) != Token::EndOfFile)
     {
-        // std::cout << "Token encontrado: " << tokenToString(token) << std::endl;
-
         if (token == Token::Error)
         {
             std::cerr << "Error: Token no reconocido" << std::endl;
